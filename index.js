a9c4fb13db21549b00ed4a1f69a511785c775de7
Mark Yun <only.hyeok@gmail.com>
2022-02-14 15:41:50 +0900

    applied lambda

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { request, gql } = require('graphql-request');
 const URL = 'https://api.wedives.com/graphql';
 
-main().catch((error) => console.error(error))
+exports.handler = async (event) => {
+
+    await main()
+
+    return {
+        statusCode: 200
+    };
+};
 
 async function main() {
     await completeDiving()
